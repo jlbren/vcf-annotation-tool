6d4257ef1004f904a51e4c5e18edd74f2b7632e0
Jonathon Brenner <jbrenner@luc.edu>
2017-09-11 01:47:25 -0500

    moved split_into_fields into annotate module

--- vcf_parser.py
+++ vcf_parser.py
@@ -21,21 +21,4 @@ class VCFParser:
                                       names = self.meta_data.headers,
                                       skiprows = self.meta_data.meta_data_length)
 
-    def split_info_field(self, line):
-        """Method for splitting a VCF info field into a dict of its keys and values.
-        Arguments:
-            line (str): Single info entry to be split into a dictionary.
-        Returns:
-            info (dict): Dictionary containing each info field as a key 
-                         with its corresponding value.
-        """
-        #Get key-value pairs. 
-        pairs = line.split(';')
-        info = {}
-        for pair in pairs:
-            field = pair.split('=')
-            # Add key-value to dictionary 
-            info[field[0]] = field[1]
-
-        return info
-
+  
