831711fc8ea119d6351c75579ad542bf07d5833a
jlbren <jbrenluc@gmail.com>
2017-09-11 05:02:07 -0500

    working on annotate. TODO: f: figure out allele freq counts, add api code

--- variant_annotation_tool.py
+++ variant_annotation_tool.py
@@ -1,16 +1,27 @@
-from vcf_parser import VCFParser
-import argparser 
+from vcf_annotate import VCFAnnotate
+from argparse import ArgumentParser
+import os
 
-  def _parse_arguments(self):
-        arg_parser = argparse.ArgumentParser(description='VCF Annotation Tool',
-                                             prog='VCFAnnotate')
-        arg_parser.add_argument('input_file', nargs=1, required=True,
-                                help='Input VCF file to be annotated.')
-        arg_parser.add_argument('--local-only', action='store_true', default=False,
-                                help='Optional flag to skip calls to ExAC API.\n'
-                                     'Missing values will be marked as NA.')
-        arg_parser.add_argument('-o', '--output', default=os.getcwd()
-                                help='Optional flag to specify output directory.\n'
-                                     'Defaults to current working directory.')
+def _parse_arguments():
+    arg_parser = ArgumentParser(description='VCF Annotation Tool',
+                                          prog='VCFAnnotate')
+    arg_parser.add_argument('input_file', nargs=1,
+                             help='Input VCF file to be annotated.')
+    arg_parser.add_argument('--local-only', action='store_true', default=False,
+                             help='Optional flag to skip calls to ExAC API.\n'
+                                  'Missing values will be marked as NA.')
+    arg_parser.add_argument('-o', '--output', default=os.getcwd(),
+                             help='Optional flag to specify output directory.\n'
+                                  'Defaults to current working directory.')
 
-        return arg_parser.parse_args()
+    return arg_parser.parse_args()
+
+def main():
+    args = _parse_arguments()
+    # Create a new VCFAnnotate object.
+    va = VCFAnnotate(args.input_file[0], args.local_only, args.output)
+    # Parse VCF file. 
+    va.parse()
+    va.annotate()
+main()
+
--- vcf_annotate.py
+++ vcf_annotate.py
@@ -1,32 +1,72 @@
 from vcf_parser import VCFParser
 import os
+import pandas
 
 class VCFAnnotate:
-    def __init__(self, vcf_file, local_only):
+    def __init__(self, vcf_file, local_only, output):
         self.file_name = vcf_file
         self.skip_api = local_only
+        self.out_path = output
 
     def parse(self):
-        # Create a VCFParser object from the input file. 
+        """Wrapper method for creating a VCFParser object"""
+        # Create a VCFParser object from the input file.
         self.vcf_data = VCFParser(self.file_name)
-    
+
     def annotate(self):
-            
+        out_name = 'annotated_' + self.vcf_data.meta_data.file_name
+        self.out_file = os.path.join(self.out_path, out_name)
+        variant_type = []
+        depth_per_base = []
+        alt_obs = []
+        percent_variant = []
+
+        for row in self.vcf_data.vcf_df['INFO']:
+            fields = self.split_info_field(row)
+            variant_type.append(fields['TYPE'])
+            depth_per_base.append(fields['DPB'])
+            # Replace commas w periods to sanitize malformed floats.
+            var_alleles = fields['AO']
+            ref_alleles = int(fields['RO'])
+            # Sum observations for multiple variants.
+            if var_alleles.find(',') > -1:
+                # Split list of observations and map to int.
+                var_list = map(int, var_alleles.split(','))
+                # Sum observations.
+                var_alleles = sum(var_list)
+            else:
+                var_alleles = int(var_alleles)
+            alt_obs.append(var_alleles)
+            # Catch divide by zero errors where there are no reference observations
+            try:
+                var_vs_ref = float(var_alleles / ref_alleles)
+            except ZeroDivisionError:
+                # Percent variant is 100% where no ref obs where found.
+                var_vs_ref = 1.00
+
+             # Convert back to string and format to two decimals.
+            percent_variant.append( '%.2f' % var_vs_ref)
+
+        fields_df = pandas.DataFrame({'TYPE': variant_type,
+                                      'DBP': depth_per_base,
+                                      'AO': alt_obs,
+                                      'AO/RO': percent_variant})
+        print(fields_df)
 
     def split_info_field(self, line):
         """Method for splitting a VCF info field into a dict of its keys and values.
         Arguments:
             line (str): Single info entry to be split into a dictionary.
         Returns:
-            info (dict): Dictionary containing each info field as a key 
+            info (dict): Dictionary containing each info field as a key
                          with its corresponding value.
         """
-        #Get key-value pairs. 
+        #Get key-value pairs.
         pairs = line.split(';')
         info = {}
         for pair in pairs:
             field = pair.split('=')
-            # Add key-value to dictionary 
+            # Add key-value to dictionary
             info[field[0]] = field[1]
 
         return info
