8072b28c6261b29886340d1b26e46b29026e65ce
Jonathon Brenner <jbrenner@luc.edu>
2017-09-11 02:14:14 -0500

    moved argparser to main script

--- vcf_annotate.py
+++ vcf_annotate.py
@@ -1,5 +1,4 @@
 from vcf_parser import VCFParser
-import argparse
 import os
 
 class VCFAnnotate:
@@ -7,20 +6,6 @@ class VCFAnnotate:
         self.file_name = vcf_file
         self.skip_api = local_only
 
-
-    def _parse_arguments(self):
-        arg_parser = argparse.ArgumentParser(description='VCF Annotation Tool',
-                                             prog='VCFAnnotate')
-        arg_parser.add_argument('input_file', nargs=1, required=True,
-                                help='Input VCF file to be annotated.')
-        arg_parser.add_argument('--local-only', action='store_true', default=False,
-                                help='Optional flag to skip calls to ExAC API.\n'
-                                     'Missing values will be marked as NA.')
-        arg_parser.add_argument('-o', '--output', default=os.getcwd()
-                                help='Optional flag to specify output directory.\n'
-                                     'Defaults to current working directory.')
-
-        return arg_parser.parse_args()
     def parse(self):
         # Create a VCFParser object from the input file. 
         self.vcf_data = VCFParser(self.file_name)
